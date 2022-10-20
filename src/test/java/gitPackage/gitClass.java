429c1f0153fdb244fb823ae60cde67052475de0e
hasannugr <114023014+hasannugr@users.noreply.github.com>
2022-10-20 23:01:33 +0300

    bilemiyorum altan

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -4,6 +4,14 @@ public class gitClass {
 
     public static void main(String[] args) {
         System.out.println("git çalismalari kapsaminda olusturuldu");
+
+        System.out.println(" ikinci satirinda ceistli kodlar yaziyorum");
+
+        String name = "hasan";
+        String surname = "huseyin";
+        int i=10;
+
+
     }
 
 
