38b3e7d1fe565921c27a84734574532e004b9732
hasannugr <114023014+hasannugr@users.noreply.github.com>
2022-10-20 23:45:42 +0300

    Added new information-2

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -5,13 +5,15 @@ public class gitClass {
     public static void main(String[] args) {
         System.out.println("git çalismalari kapsaminda olusturuldu");
 
-        System.out.println(" ikinci satirinda ceistli kodlar yaziyorum");
+        System.out.println(" ikinci satirinda cesitli kodlar yaziyorum");
 
         String name = "hasan";
         String surname = "huseyin";
         int i=10;
 
         String hesapBilgileri= "27092015";
+        String esDruumu="tövbekar";
+        boolean cocuk=false;
 
         String adres= "NEF ARENA";
         String il = "İSTANBUL";
