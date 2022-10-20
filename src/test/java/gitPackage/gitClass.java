95bf4e7c671cde85187c421173306b5ae19800e2
hasannugr <114023014+hasannugr@users.noreply.github.com>
2022-10-20 23:24:47 +0300

    Change adress

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -11,7 +11,7 @@ public class gitClass {
         String surname = "huseyin";
         int i=10;
 
-        String adres= "NEF ARENA GALATASARAY";
+        String adres= "NEF ARENA";
         String il = "İSTANBUL";
 
 
