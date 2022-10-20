33142c9d93da8c62a328aae52b2b9607c2833da4
hasannugr <114023014+hasannugr@users.noreply.github.com>
2022-10-20 23:27:56 +0300

    Deleted all information

--- src/test/java/gitPackage/gitClass.java
+++ src/test/java/gitPackage/gitClass.java
@@ -3,18 +3,6 @@ package gitPackage;
 public class gitClass {
 
     public static void main(String[] args) {
-        System.out.println("git çalismalari kapsaminda olusturuldu");
-
-        System.out.println(" ikinci satirinda ceistli kodlar yaziyorum");
-
-        String name = "hasan";
-        String surname = "huseyin";
-        int i=10;
-
-        String adres= "NEF ARENA";
-        String il = "İSTANBUL";
-
-
 
     }
 
